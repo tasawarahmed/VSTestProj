88095c24d17baa5c874dde702d7c8a32584c0afb
tasawarahmed <49127897+tasawarahmed@users.noreply.github.com>
2021-09-22 10:01:28 +0500

    Menu Changed

--- IttefaqConstructionServices/Site.Master.designer.cs
+++ IttefaqConstructionServices/Site.Master.designer.cs
@@ -229,13 +229,31 @@ namespace IttefaqConstructionServices {
         protected global::System.Web.UI.WebControls.HyperLink HyperLink44;
         
         /// <summary>
-        /// HyperLink45 control.
+        /// HyperLink1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink45;
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
+        /// HyperLink2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        
+        /// <summary>
+        /// HyperLink3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
         
         /// <summary>
         /// HyperLink52 control.
